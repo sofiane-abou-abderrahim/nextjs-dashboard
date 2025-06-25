66b1c3e87d09a7078a40c63ab88ff63a6fd03220
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2025-06-25 02:36:22 +0100

    16: Adding Metadata

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -8,6 +8,11 @@ import {
   RevenueChartSkeleton,
   CardsSkeleton,
 } from "@/app/ui/skeletons";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Admin",
+};
 
 export default async function Page() {
   return (
